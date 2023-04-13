20863f1d65b18e15903edcefc96a390661f9e740
Julia Khabibova <julia.khabibova@mail.ru>
2023-04-13 20:57:39 +0500

    first

--- scripts/index.js
+++ scripts/index.js
@@ -15,7 +15,7 @@ buttonClose.addEventListener('click', closePopup);
 
 const profileName = document.querySelector('.profile__title');
 const profileJob = document.querySelector('.profile__subtitle');
-const formElement = document.querySelector('.popup__input');
+const formElement = document.querySelector('.popup__content');
 const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_job');
 
