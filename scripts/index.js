40f7f1896cde1137d5d525b50c5b79138359fddc
Julia Khabibova <julia.khabibova@mail.ru>
2023-04-18 20:56:09 +0500

    third

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 const buttonEdit = document.querySelector('.profile__edit-button');
 const buttonClose = document.querySelector('.popup__close');
+const buttonCloseadd = document.querySelector('.popup__close_add');
 const popup = document.querySelector('.popup');
 const profileName = document.querySelector('.profile__title');
 const profileJob = document.querySelector('.profile__subtitle');
@@ -7,6 +8,14 @@ const formElement = document.querySelector('.popup__content');
 const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_job');
 
+const buttonAdd = document.querySelector('.profile__add-button');
+const add = document.querySelector('.popup_add');
+
+function openPopupadd() {
+    add.classList.add('popup__visible');
+}
+
+buttonAdd.addEventListener('click', openPopupadd);
 
 function openPopup() {
     popup.classList.add('popup__visible');
@@ -14,6 +23,10 @@ function openPopup() {
 
 function closePopup() {
     popup.classList.remove('popup__visible');
+}
+
+function closePopupadd() {
+    add.classList.remove('popup__visible');
 }
 
 function handleFormSubmit (evt) {
@@ -25,4 +38,5 @@ function handleFormSubmit (evt) {
 
 buttonEdit.addEventListener('click', openPopup);
 buttonClose.addEventListener('click', closePopup);
+buttonCloseadd.addEventListener('click', closePopupadd);
 formElement.addEventListener('submit', handleFormSubmit); 
